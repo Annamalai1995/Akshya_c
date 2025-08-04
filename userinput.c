8bbc7b9a93c0d6bea381da11cb53d5a9e71766e0
Annamalai1995 <s.annamalai1995@gmail.com>
2025-08-04 11:03:55 +0530

    scanf function

---
+++ userinput.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int alpha=0;
+    int beta=0;
+    scanf("%d %d",&alpha,&beta);
+    int sum=alpha+beta;
+    printf("The value is %d",sum);
+
+    // int data=100;
+    // int data1=150;
+    // int sum=data+data1;
+    // printf("The Sum of values %d and %d is %d\n",data,data1,sum);
+    //printf("Sum of values %d",sum);
+
+}
+
