2b62b0b9721b3135ae65218b48bb98a8119d0ec5
Annamalai1995 <s.annamalai1995@gmail.com>
2025-09-17 10:59:48 +0530

    palindrome and factorial

---
+++ pallindrome.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int i, length, Palindrome = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    length = strlen(str);
+
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            Palindrome = 0;
+            break;
+        }
+    }
+
+    if (Palindrome)
+        printf("%s is a palindrome.\n", str);
+    else
+        printf("%s is not a palindrome.\n", str);
+
+    return 0;
+}
